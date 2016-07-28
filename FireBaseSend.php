08ac21582c064d76c9ce5f55ede22db9ab1657cf
kraiba <krabz01@gmail.com>
2016-07-28 17:48:38 +0300

    get api from params

--- FireBaseSend.php
+++ FireBaseSend.php
@@ -2,13 +2,21 @@
 
 namespace kraiba\fcb;
 
+use Yii;
 /**
  * This is just an example.
  */
 class FirebaseSend
 {
+	var api_key;
+	    // constructor
+    function __construct() {
+        $this->api_key=Yii::$app->params['fcm']["apiKey"]
+    }
+
     public function sendMessage()
     {
+    	
         return "Hello!";
     }
 }
